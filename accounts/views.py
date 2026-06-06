31eb8a552c87ea0f0f2e7f6f2f026606ee39bcc6
idulik <idulik@gmail.com>
2026-06-06 07:42:11 +0200

    registracia

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# REGISTRÁCIA
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("game")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "register.html", {"form": form})
+
 
 # Create your views here.
--- piskvorky/urls.py
+++ piskvorky/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+
     path("", include("game.urls")),
+    path("accounts/", include("accounts.urls")),
+
+    # LOGIN / LOGOUT
+    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
