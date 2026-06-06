f67b571d34838a8a2d3812e3de28c6f156cdf348
idulik <idulik@gmail.com>
2026-06-06 16:04:08 +0200

    oprava tlacitla restartu hry na home aj game

--- game/views.py
+++ game/views.py
@@ -298,7 +298,12 @@ def reset(request):
     ]
     request.session["winner"] = None
     request.session["last_ai_move"] = None
-
     request.session.modified = True
+
+    source = request.GET.get("from")
+
+    if source == "home":
+        return redirect("home")
+
     return redirect("game")
 
