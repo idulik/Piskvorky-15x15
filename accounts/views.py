93e409547c3eef253ae53228260d703107836868
idulik <idulik@gmail.com>
2026-06-06 13:41:50 +0200

    vlastny formular + email

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,16 @@
 # REGISTRÁCIA
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth import login
 from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect("game")
+            form.save()
+            return redirect("login")
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
 
     return render(request, "register.html", {"form": form})
-
-
-# Create your views here.
