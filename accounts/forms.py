2d4b6915a86d97e6eb8d2ece6d8d581eeefdfd3d
idulik <idulik@gmail.com>
2026-06-08 08:52:18 +0200

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,12 +3,21 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+
+        if email and User.objects.filter(email=email).exists():
+            raise ValidationError("Email už existuje")
+
+        return email
 
     def save(self, commit=True):
         user = super().save(commit=False)
@@ -18,11 +27,3 @@ class RegisterForm(UserCreationForm):
             user.save()
 
         return user
-    
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-
-        if User.objects.filter(email=email).exists():
-            raise ValidationError("Email už existuje")
-
-        return email
